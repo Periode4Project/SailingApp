d31a9e18a1bfa53e5c0aa09b4238a7727cff895b
KungFuKevin <kevin.voet@student.nhlstenden.com>
2021-05-19 14:05:26 +0200

    Added Navigation Bar with functional navigation, (Background)Colours.

--- Sailing/App.xaml.cs
+++ Sailing/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Sailing
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new AppShell();
         }
 
         protected override void OnStart()
