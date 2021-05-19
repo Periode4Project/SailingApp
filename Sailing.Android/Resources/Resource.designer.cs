56332f1a7942c6ddc9dfc723144360d496afa46d
KungFuKevin <kevin.voet@student.nhlstenden.com>
2021-05-19 16:06:21 +0200

    Added Main Page Design with an example output.

--- Sailing.Android/Resources/Resource.designer.cs
+++ Sailing.Android/Resources/Resource.designer.cs
@@ -6266,19 +6266,22 @@ namespace Sailing.Android
 			public const int notify_panel_notification_icon_bg = 2131165499;
 			
 			// aapt resource value: 0x7F07013C
-			public const int SearchIcon = 2131165500;
+			public const int RamseysExample = 2131165500;
 			
 			// aapt resource value: 0x7F07013D
-			public const int Settings = 2131165501;
+			public const int SearchIcon = 2131165501;
 			
 			// aapt resource value: 0x7F07013E
-			public const int ShipIcon = 2131165502;
+			public const int Settings = 2131165502;
 			
 			// aapt resource value: 0x7F07013F
-			public const int tooltip_frame_dark = 2131165503;
+			public const int ShipIcon = 2131165503;
 			
 			// aapt resource value: 0x7F070140
-			public const int tooltip_frame_light = 2131165504;
+			public const int tooltip_frame_dark = 2131165504;
+			
+			// aapt resource value: 0x7F070141
+			public const int tooltip_frame_light = 2131165505;
 			
 			static Drawable()
 			{
