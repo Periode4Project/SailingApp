56da837592285b553b508ecb1d1ea4591235ae3c
pjorter <71328059+pjorter@users.noreply.github.com>
2021-05-25 21:28:54 +0200

    Model en viewmodel voor bindings, viewmodel zelf moet nog gemaakt worden. Front-end verbonden aan viewmodel.

---
+++ Sailing/Activity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sailing
+{
+    class Activity
+    {
+        public string ActivityImage { get; set; }
+
+        public string ActivityName { get; set; }
+
+        public string ActivityDesc { get; set; }
+
+        public string ActivityType { get; set; }
+
+        public string ActivityLocation { get; set; }
+
+    }
+}
---
+++ Sailing/ViewModels/ActivityViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Sailing.ViewModels
+{
+    public class ActivityViewModel
+    {
+
+    }
+}
