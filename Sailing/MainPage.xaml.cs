bd4bebb602d1a9a11c5a791f2f614b4f82971337
mrbutterbeach <rickwijbenga@gmail.com>
2021-06-01 14:13:05 +0200

    locatie kan worden opgehaald

--- Sailing/MainPage.xaml.cs
+++ Sailing/MainPage.xaml.cs
@@ -4,14 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace Sailing
 {
     public partial class MainPage : ContentPage
     {
+        bool isGettingLocation;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            isGettingLocation = true;
+
+            while (isGettingLocation)
+            {
+                var result = await Geolocation.GetLocationAsync(new GeolocationRequest(GeolocationAccuracy.Default, TimeSpan.FromMinutes(1)));
+
+                resultLocation.Text += $"lat: {result.Latitude}, lng: {result.Longitude}{Environment.NewLine}";
+
+                await Task.Delay(1000);
+            }
+        }
+
+        void Button1_Clicked(System.Object sender, System.EventArgs e)
+        {
+            isGettingLocation = false;
+        }
+
+        async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            var result = await Geolocation.GetLocationAsync(new GeolocationRequest(GeolocationAccuracy.Default, TimeSpan.FromMinutes(1)));
+
+            try
+            {
+                var lat = result.Latitude;
+                var lon = result.Longitude;
+
+                var placemarks = await Geocoding.GetPlacemarksAsync(lat, lon);
+
+                var placemark = placemarks?.FirstOrDefault();
+                if (placemark != null)
+                {
+                    var geocodeAddress =
+                        $"AdminArea:       {placemark.AdminArea}\n" +
+                        $"CountryCode:     {placemark.CountryCode}\n" +
+                        $"CountryName:     {placemark.CountryName}\n" +
+                        $"FeatureName:     {placemark.FeatureName}\n" +
+                        $"Locality:        {placemark.Locality}\n" +
+                        $"PostalCode:      {placemark.PostalCode}\n" +
+                        $"SubAdminArea:    {placemark.SubAdminArea}\n" +
+                        $"SubLocality:     {placemark.SubLocality}\n" +
+                        $"SubThoroughfare: {placemark.SubThoroughfare}\n" +
+                        $"Thoroughfare:    {placemark.Thoroughfare}\n";
+
+                    Console.WriteLine(geocodeAddress);
+                    cityResult.Text = geocodeAddress;
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature not supported on device
+            }
+            catch (Exception ex)
+            {
+                // Handle exception that may have occurred in geocoding
+            }
         }
     }
 }
