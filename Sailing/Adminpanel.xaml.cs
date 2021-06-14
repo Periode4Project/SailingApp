2fd5c239b293e41cce2b2efaf4d63ddf00e9e1fa
pjorter <71328059+pjorter@users.noreply.github.com>
2021-06-14 12:48:03 +0200

    pages gemaakt en de basis van het adminpaneel.

---
+++ Sailing/Adminpanel.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sailing
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Adminpanel : ContentPage
+    {
+        public Adminpanel()
+        {
+            InitializeComponent();
+        }
+        private async void Activity_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AdminModerateActivity());
+        }
+
+        private async void Review_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AdminModerateReview());
+
+        }
+        private async void Mail_Clicked(object sender, EventArgs e)
+        {
+            string url = "https://mail.google.com/";
+            await Launcher.OpenAsync(new Uri(url));
+        }
+    }
+}
--- Sailing/MyAccount.xaml.cs
+++ Sailing/MyAccount.xaml.cs
@@ -27,5 +27,9 @@ namespace Sailing
             await Navigation.PushAsync(new Register());
 
         }
+        private async void Admin_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Adminpanel());
+        }
     }
 }
