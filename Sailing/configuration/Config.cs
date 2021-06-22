8196d1d7192689e446a3c551d5a0e5f7be28bd96
dennisrijsdijk <dennis.rijsdijk@student.nhlstenden.com>
2021-06-22 13:09:40 +0200

    Config handling

---
+++ Sailing/configuration/Config.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Sailing
+{
+    public static class Config
+    {
+        public static User User { get; set; }
+        public static bool FirstStartup { get; set; }
+        static Config()
+        {
+            User = new User
+            {
+                Email = null,
+                Password = null
+            };
+            FirstStartup = false;
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            string filename = Path.Combine(path, "config.json");
+            //define the string, so it can be used inside the streamreader loop
+            string content;
+
+            //Check if the file exists. If not, create it. it'll return "" due to there being no API key, which is handled in the key verification process
+            if (!File.Exists(filename))
+            {
+                string json = JsonConvert.SerializeObject(User, Formatting.Indented);
+                using (StreamWriter outFile = new StreamWriter(filename))
+                {
+                    outFile.Write(json);
+                }
+                FirstStartup = true;
+            }
+
+            //properly handle Streamreader
+            using (StreamReader file = File.OpenText(@"config.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                User = (User)serializer.Deserialize(file, typeof(User));
+            }
+
+        }
+    }
+}
