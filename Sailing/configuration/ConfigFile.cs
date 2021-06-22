1d0d312c3f7739e00be4078138693c95fd0d2e4f
dennisrijsdijk <dennis.rijsdijk@student.nhlstenden.com>
2021-06-22 13:16:22 +0200

    ConfigFile Class

---
+++ Sailing/configuration/ConfigFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sailing.configuration
+{
+    public class ConfigFile
+    {
+        public string Language { get; set; }
+        public User User { get; set; }
+    }
+}
